8b5b898a7e8fe5d0be3557c417d08ff96bef0956
Shankar Vellal <vidyashankar.mv@gmail.com>
2016-03-27 21:05:41 -0400

    add ELB health check

--- go/src/main/main.go
+++ go/src/main/main.go
@@ -7,14 +7,18 @@ import (
 
 func indexHandler (w http.ResponseWriter, r *http.Request) {
 	if r.Header.Get("X-Forwarded-Proto") == "https" {
-		fmt.Fprintf(w, "Hello, World!")
+		fmt.Fprintf(w, "Hello, World! Secure!")
+	} else {	
+		http.Redirect(w, r, "https://shankarvellal.com:443"+r.RequestURI, http.StatusMovedPermanently)
 	}
-	else {	
-		http.Redirect(w, r, "https://shankarvellal.com"+r.RequestURI, http.StatusMovedPermanently)
-	}
+}
+
+func elbHealthCheck (w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Health OK!")
 }
 
 func main() {
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/elb_health_check", elbHealthCheck)
 	http.ListenAndServe(":8080", nil)
 }
